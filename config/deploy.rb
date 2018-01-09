a966c561ea3abee0ff0ea0b10a4092937a01b94e
penguin-san <itosouplus@gmail.com>
2018-01-09 10:32:02 +0900

    deploy automation file update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
+#lock "~> 3.10.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+#set :application, "my_app_name"
+#set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -40,7 +40,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 #
 #
 #
-require 'rvm/capistrano'
+#require 'rvm/capistrano'
 lock '3.2.1'
 
 set :application, 'sky_sample'
